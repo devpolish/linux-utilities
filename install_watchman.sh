644cf3053e3927381b98c252f80a54a898f78e08
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-04-14 01:53:15 -0500

    Create install_watchman.sh

---
+++ install_watchman.sh
@@ -0,0 +1,12 @@
+cd ~
+git clone https://github.com/facebook/watchman.git
+cd watchman/
+git checkout v4.7.0
+sudo apt-get install -y autoconf automake build-essential python-dev
+./autogen.sh 
+./configure 
+make
+sudo make install
+
+watchman --version
+echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_user_watches  && echo 999999 | sudo tee -a  /proc/sys/fs/inotify/max_queued_events && echo 999999 | sudo tee  -a /proc/sys/fs/inotify/max_user_instances && watchman  shutdown-server
